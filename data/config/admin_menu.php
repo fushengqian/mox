8772c174ad7ef554c3047ce0b0cd327493fb92be
fushengqian <fushengqian@qq.com>
2019-05-27 14:05:27 +0800

    fixed 二期日常开发

--- data/config/admin_menu.php
+++ data/config/admin_menu.php
@@ -8,44 +8,76 @@ $config[] = array(
 
 $config[] = array(
     'title'    => MOX_APP::lang()->_t('内容管理'),
-    'cname'    => 'activity',
+    'cname'    => 'topic',
     'children' => array(
         array(
-            'id' => 804,
-            'title' => MOX_APP::lang()->_t('话题管理'),
-            'url' => '/backend/topic/index/'
+            'id' => 101,
+            'title' => MOX_APP::lang()->_t('广告管理'),
+            'url' => '/backend/article/list/'
         ),
         array(
-            'id' => 803,
-            'title' => MOX_APP::lang()->_t('动态管理'),
-            'url' => '/backend/feed/index/'
-        ),
-        array(
-            'id' => 802,
+            'id' => 102,
             'title' => MOX_APP::lang()->_t('文章管理'),
             'url' => '/backend/article/list/'
         ),
         array(
-            'id' => 801,
+            'id' => 103,
             'title' => MOX_APP::lang()->_t('短信管理'),
             'url' => '/backend/sms/list/'
-        ),
+        )
     )
 );
 
 $config[] = array(
-    'title'    => MOX_APP::lang()->_t('模型管理'),
-    'cname'    => 'plus',
+    'title'    => MOX_APP::lang()->_t('社区管理'),
+    'cname'    => 'activity',
     'children' => array(
         array(
-            'id' => 100,
-            'title' => MOX_APP::lang()->_t('品牌管理'),
-            'url' => '/backend/brand/index/'
+            'id' => 201,
+            'title' => MOX_APP::lang()->_t('社区设置'),
+            'url' => '/backend/topic/setting/'
         ),
         array(
-            'id' => 101,
-            'title' => MOX_APP::lang()->_t('模型管理'),
-            'url' => '/backend/model/index/'
+            'id' => 202,
+            'title' => MOX_APP::lang()->_t('话题管理'),
+            'url' => '/backend/topic/index/'
+        ),
+        array(
+            'id' => 203,
+            'title' => MOX_APP::lang()->_t('动态管理'),
+            'url' => '/backend/feed/index/'
+        )
+    )
+);
+
+$config[] = array(
+    'title'    => MOX_APP::lang()->_t('商城管理'),
+    'cname'    => 'cart',
+    'children' => array(
+        array(
+            'id' => 'SETTINGS_MALL',
+            'title' => MOX_APP::lang()->_t('商城设置'),
+            'url' => '/backend/main/settings/category-mall'
+        ),
+        array(
+            'id' => 302,
+            'title' => MOX_APP::lang()->_t('分类管理'),
+            'url' => '/backend/category/index/'
+        ),
+        array(
+            'id' => 303,
+            'title' => MOX_APP::lang()->_t('商品管理'),
+            'url' => '/backend/goods/index/'
+        ),
+        array(
+            'id' => 304,
+            'title' => MOX_APP::lang()->_t('促销管理'),
+            'url' => '/backend/promotion/index/'
+        ),
+        array(
+            'id' => 303,
+            'title' => MOX_APP::lang()->_t('分销管理'),
+            'url' => '/backend/goods/index/'
         ),
     )
 );
@@ -55,55 +87,68 @@ $config[] = array(
     'cname'    => 'user',
     'children' => array(
         array(
-            'id' => 700,
+            'id' => 401,
             'title' => MOX_APP::lang()->_t('用户列表'),
             'url' => '/backend/user/list/'
         ),
         array(
-            'id' => 701,
-            'title' => MOX_APP::lang()->_t('用户行为'),
+            'id' => 402,
+            'title' => MOX_APP::lang()->_t('用户日志'),
             'url' => '/backend/action/index/'
         ),
+        array(
+            'id' => 403,
+            'title' => MOX_APP::lang()->_t('用户组管理'),
+            'url' => '/backend/user/group/'
+        ),
     )
 );
 
 $config[] = array(
-    'title'    => MOX_APP::lang()->_t('运营管理'),
-    'cname'    => 'edit',
+    'title'    => MOX_APP::lang()->_t('评论管理'),
+    'cname'    => 'comment',
     'children' => array(
         array(
-            'id' => 902,
-            'title' => MOX_APP::lang()->_t('日志监控'),
+            'id' => 501,
+            'title' => MOX_APP::lang()->_t('评论列表'),
+            'url' => '/backend/comment/list/'
+        )
+    )
+);
+
+$config[] = array(
+    'title'    => MOX_APP::lang()->_t('系统设置'),
+    'cname'    => 'setting',
+    'children' => array(
+        array(
+            'id' => 'SETTINGS_SITE',
+            'title' => MOX_APP::lang()->_t('站点信息'),
+            'url' => '/backend/main/settings/'
+        ),
+        array(
+            'id' => 'SETTINGS_FUNCTIONS',
+            'title' => MOX_APP::lang()->_t('站点功能'),
+            'url' => '/backend/main/settings/category-functions'
+        ),
+        array(
+            'id' => 'SETTINGS_NAV',
+            'title' => MOX_APP::lang()->_t('导航菜单'),
+            'url' => '/backend/main/settings/category-nav'
+        ),
+        array(
+            'id' => 601,
+            'title' => MOX_APP::lang()->_t('清除缓存'),
+            'url' => '/backend/cache/clear/'
+        ),
+        array(
+            'id' => 602,
+            'title' => MOX_APP::lang()->_t('后台日志'),
             'url' => '/backend/logs/index/'
         ),
-    )
-);
-
-$config[] = array(
-    'title'    => MOX_APP::lang()->_t('城市管理'),
-    'cname'    => 'draft',
-    'children' => array(
         array(
-            'id' => 400,
-            'title' => MOX_APP::lang()->_t('城市列表'),
-            'url' => '/backend/city/index/'
-        ),
-        array(
-            'id' => 401,
-            'title' => MOX_APP::lang()->_t('地区列表'),
-            'url' => '/backend/area/index/'
-        ),
-    )
-);
-
-$config[] = array(
-    'title'    => MOX_APP::lang()->_t('系统管理'),
-    'cname'    => 'inviteask',
-    'children' => array(
-        array(
-            'id' => 600,
-            'title' => MOX_APP::lang()->_t('清除搜索缓存'),
-            'url' => '/backend/search/clear/'
-        ),
+            'id' => 603,
+            'title' => MOX_APP::lang()->_t('SEO设置'),
+            'url' => '/backend/seo/list/'
+        )
     )
 );
--- data/config/system.php
+++ data/config/system.php
@@ -1,27 +1,33 @@
 <?php
-$config['debug'] = false;
+/**
++--------------------------------------------------------------------------
+|   Mox
+|   ========================================
+|   by Mox Software
+|   © 2018 - 2019 Mox. All Rights Reserved
+|   http://www.moxquan.com
+|   ========================================
+|   Support: 540335306@qq.com
+|   Author: FSQ
++---------------------------------------------------------------------------
+*/
 
 // 定义 Cookies 作用域
-define('G_COOKIE_DOMAIN', '.moxquan.cn');
+define('G_COOKIE_DOMAIN','');
 
 // 定义 Cookies 前缀
-define('G_COOKIE_PREFIX', 'mox_');
+define('G_COOKIE_PREFIX','fsc_');
 
 // 定义应用加密 KEY
-define('G_SECUKEY', 'xmkbfrj2xkbg');
-define('G_COOKIE_HASH_KEY', 'yjklauro1fcksol');
+define('G_SECUKEY','thkpcvdhnrvt');
+define('G_COOKIE_HASH_KEY', 'udacadqxzyehylo');
 
-define('G_INDEX_SCRIPT', '/');
+define('G_INDEX_SCRIPT', '?/');
 
 define('X_UA_COMPATIBLE', 'IE=edge,Chrome=1');
 
 // GZIP 压缩输出页面
-define('G_GZIP_COMPRESS', true);
-
-// 是否缓存
-define('G_CACHE', true);
-
-define('G_ONLINE', false);
+define('G_GZIP_COMPRESS', FALSE);
 
 // Session 存储类型 (db, file)
 define('G_SESSION_SAVE', 'db');
@@ -32,16 +38,12 @@ define('G_SESSION_SAVE_PATH', '');
 define('G_BASE_DEMAIN', '.moxquan.cn');
 
 $base_url = explode('.', $_SERVER['SERVER_NAME']);
-if ($base_url[0] == 'm')
-{
+if ($base_url[0] == 'm') {
     define('G_DEMAIN', 'http://m.moxquan.cn');
-}
-else
-{
+} else {
     define('G_DEMAIN', 'http://www.moxquan.cn');
 }
 
-define('SITE_NAME', '模型圈');
+define('SITE_NAME', 'Mox');
 
-define('G_STATIC', 'http://www.moxquan.cn/static');
-
+define('G_STATIC', G_DEMAIN.'/static');
